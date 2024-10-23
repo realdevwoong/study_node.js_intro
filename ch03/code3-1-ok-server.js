410f9156024c2cb0dd2779c3ba607298862cbb8e
realdevwoong <projung134@gmail.com>
2024-10-23 23:00:05 +0900

    í³

--- ch03/code3-1-ok-server.js
+++ ch03/code3-1-ok-server.js
@@ -18,7 +18,8 @@ http.createServer((req, res) => {
 }).listen(3000, () => console.log("라우터를 만들어보자"));
 
 const user = (req, res) => {
-    res.end(`[user] name: andy, age: 30`);
+    const userInfo = url.parse(req.url, true).query;
+    res.end(`[user] name: ${userInfo.name}, age: ${userInfo.age}`);
 }
 
 const feed = (req, res) => {
