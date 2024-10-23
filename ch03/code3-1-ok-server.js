a3a67f9d62b29eeb2b8bc1c6eb6dfd6287cc49de
realdevwoong <projung134@gmail.com>
2024-10-23 22:43:33 +0900

    Comment: "path 변수 설명 주석 추가"

--- ch03/code3-1-ok-server.js
+++ ch03/code3-1-ok-server.js
@@ -3,6 +3,9 @@ const url = require("url");
 
 http.createServer((req, res) => {
     const path = url.parse(req.url, true).pathname;
+    //urlString: 파싱할 URL 문자열입니다. 이 부분은 필수 인자이며, 예를 들어 http://example.com/path?name=value와 같은 형식이 됩니다.
+    //parseQueryString: (선택적) 이 매개변수가 true로 설정되면 쿼리 문자열이 객체 형태로 파싱됩니다. 기본값은 false이며, 이 경우 쿼리 문자열은 문자열 그대로 반환됩니다.
+    //예를 들어:쿼리 문자열: ?name=value&age=30 true를 주면 { name: 'value', age: '30' } 형태의 객체로 반환됩니다.false를 주면 'name=value&age=30'라는 문자열로 반환됩니다.
     res.setHeader("Content-Type", "text/html; charset=utf-8");
 
     if (path === "/user") {
