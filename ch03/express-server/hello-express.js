7c094ecc8d1e85b25480b03364849a72ac9f2f89
realdevwoong <projung134@gmail.com>
2024-10-24 21:32:46 +0900

    Refactor: "express로 이전 코드 구현하기"

--- ch03/express-server/hello-express.js
+++ ch03/express-server/hello-express.js
@@ -4,20 +4,23 @@ const app = express();
 //애플리케이션 객체 생성:
 const port = 3000;
 //서버 포트를 설정
-app.get("/", (req, res) =>{
-    res.set({'Content-Type':"text/html; charset=utf-8"});
-    res.end("헬로 Express");
-});
-//첫 번째 매개변수 "/"는 루트 경로를 의미
-//두 번째 매개변수는 콜백 함수 (req, res)입니다.
-//req: 요청 객체로, 클라이언트의 요청에 대한 정보를 담고 있음
-//res: 응답 객체로, 서버가 클라이언트에게 응답을 보낼 때 있음
-//이 콜백 함수는 클라이언트가 루트 경로에 GET 요청을 보낼 때 실행됩니다.
-//res.set() 메서드는 응답 헤더를 설정하며, 여기서는 콘텐츠 타입을 HTML로 설정하고 UTF-8 인코딩을 사용합니다.
-//res.end() 메서드는 응답을 종료하고 클라이언트에게 "헬로 Express"라는 메시지를 보냅니다.
-app.listen(port, ()=>{
-    console.log(`START SERVER: use ${port}`);
+app.listen(port,()=>{
+    console.log("익스프레스로 라우터 리팩터링하기");
 })
-//app.listen() 메서드는 지정된 포트에서 서버를 시작하고 클라이언트의 요청을 대기합니다.
-//두 번째 매개변수는 콜백 함수 ()로, 서버가 성공적으로 시작되었을 때 실행됩니다.
-//이 콜백 함수는 매개변수를 받지 않으며, 단순히 콘솔에 메시지를 출력합니다.
+app.get("/",(_,res)=>res.end("HOME"));
+//그냥 반환만 하기 때문에 _로 써줌 
+app.get("/user", user);
+app.get("/feed", feed);
+
+function usr(req,res){
+    const user = url.parse(req.url,true).query;
+    res.json(`[user] name: ${user.name}, age:${user.age}`);
+}
+function feed(_,res){
+    res.json(`<ul>
+        <li>picture1</li>
+        <li>picture2</li>
+        <li>picture3</li>
+        </ul>
+        `);
+}
